371f8b1178c56d00ecd7bb8b9b6b985c75750805
Zachary Chun <chunz2@uw.edu>
2017-09-19 15:42:26 -0700

    implemented edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -12,11 +14,23 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new
-		@article.title = params[:article][:title]
-		@article.body = params[:article][:body]
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to article_path(@article)
 	end
 
+	def destroy
+		Article.find(params[:id]).destroy
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+    redirect_to article_path(@article)
+	end
 end
